2a3d1df7b4fdb200c8878bdc8172885627f9b93d
vijay pathem <vijaypathem4@gmail.com>
2023-07-16 19:01:08 +0530

    css ands detail post view addded

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import post_list
+from .views import post_list, post_detail
 
 urlpatterns = [
     path('', post_list, name='post_list'),
+    path('post/<int:pk>/', post_detail, name='post_detail')
 ]
